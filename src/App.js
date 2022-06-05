3968f1cdbe4afb3e8bfb3cc7a78a313fa380c2a8
Nico <nicholas.computacao@outlook.com>
2022-06-05 10:29:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi {process.env.REACT_APP_NAME}
         </a>
       </header>
     </div>
